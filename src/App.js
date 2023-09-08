b189881413dc3e5fc7086af4bcc8f2756c73bdfa
ByCheno <jcoronelcamacho@gmail.com>
2023-09-08 15:32:38 +0200

    Finished

--- src/App.js
+++ src/App.js
@@ -2,25 +2,29 @@ import './App.css';
 import Contador from './componentes/Contador';
 import Boton from './componentes/Boton';
 import amLogo from './img/amLogo.webp';
+import { useState } from 'react';
 
 function App() {
 
+  const [laps, setLaps] = useState(0);
+
   const oneMoreLap = () => {
-    console.log('Probando mejoras!!');
+    setLaps(laps + 1);  
   }
 
   const resetCar = () => {
-    console.log('Car is broken');
+    setLaps(0);
   }
 
   return (
     <div className="App">
         <div className='logo-AM'>
           <img className='img-logo' src={amLogo} alt='Logo de Aston Martin'/>
+          <h1 className='encabezado'>VUELTAS DE PRUEBA PARA FERNANDO ALONSO</h1>
         </div>
         <dev className='contenedor-principal'>
-            <Contador numClics='100' />
-            <Boton text={'Probar mejoras'}
+            <Contador numClics= {laps} />
+            <Boton text={'Probar monoplaza'}
             isButtonClic={true}
             taller={oneMoreLap}/>
             <Boton text={'Volver al garaje'}
